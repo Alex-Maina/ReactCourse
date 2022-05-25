c2757b1ad78a323860852245d6189627f9ee04b4
Maina Kimani <48632817+Alex-Maina@users.noreply.github.com>
2022-05-25 23:30:31 +0300

    Create style.css

---
+++ Project 1  - Static Page/style.css
@@ -0,0 +1,46 @@
+function Header(){
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+                <h3>ReactFacts</h3>
+                <h4>React Course - Project 1</h4>
+            </nav>
+        </header> 
+    )
+}
+
+function Body(){
+    return (
+        <div className="main"> 
+            <h1>Fun facts about React</h1>
+            <ul className="list">
+                <li>Was first created in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul> 
+        </div>
+    )
+}
+
+function Footer(){
+    return (
+        <footer>
+            <small>© 2022 Maina Kimani. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return(
+        <> 
+        <Header />
+        <Body />
+        <Footer />
+        </>
+    ) 
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
